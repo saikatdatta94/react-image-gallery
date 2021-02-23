0100d71e71ce84e6597101cca3e64f29c614c438
Saikat Datta <saikatdatta94@gmail.com>
2021-02-23 22:39:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Content from "./components/Content";
 import React,{useEffect, useState} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles'; 
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles({
